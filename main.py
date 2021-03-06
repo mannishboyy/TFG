5e154185ad8129ea56ee646a9cd5e1f87ed83e51
mannishboyy <guillermo.cara.g@gmail.com>
2021-03-06 21:39:53 +0100

    ONSET DESVIATIONS NORM TO 0 AND BEGINING-END

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import csv
 import glob
-
-
-
+#THIS FUNCTION RETURNS THE ON SET VALUES TO COMPARE.
 def o_file(o_filename):
     with open(o_filename, 'r') as original:
         value_original = []
@@ -12,12 +10,83 @@ def o_file(o_filename):
             #print(row)
     return value_original
 
-def comp_file(comp_filename, w_filename):
+#THIS FUNCTION RETURNS THE ON SET VALUES TO COMPARE.
+def x_file(x_filename):
+    with open(x_filename, 'r') as original:
+        value_original = []
+        original_reader = csv.reader(original)
+        for linia in original_reader:
+            value_original.append(linia[5])
+            break
+        for row in original_reader:
+            norm = float(row[5])
+            value_original.append(float(row[5])-norm)
+            break
+        for lin in original_reader:
+            value_original.append(float(lin[5]) - norm)
+    return value_original
+
+#THIS FUNCTION COMPARES THE GIVEN VALUES BETWEEN ALL OF THE FILES
+def comp_file0(comp_filename, w_filename, value_original):
     with open(comp_filename, 'r') as compare, open(w_filename, 'w', newline='') as result:
         compare_reader = csv.reader(compare)
         result_writer = csv.writer(result)
         result_writer.writerow(['OnSet desviation'])
         fila = 1
+        next(compare_reader)
+        for linia in compare_reader:
+            if isinstance(float(linia[5]), float):
+                norm = float(linia[5])
+                result_writer.writerows(['0'])
+                break
+        for row in compare_reader:
+            fila = fila+1
+            a = float(row[5])-norm
+            b = value_original[fila]
+            resultado = a-float(b)
+            result_writer.writerow([resultado])
+
+#THIS FUNCTION MAKES ALL THE COMPARATIONS AND CREATES THE NEW CSV FILES
+def Norm0(o_filename,all_files_nmat,files_a,files_b,files_c):
+    value_original = o_file(o_filename)
+    for file in all_files_nmat:
+        comp_file0(file, 'Resultados_0/'+file[9:12]+'_o-'+file[13:14]+'.csv',value_original)
+    pos = 0
+    for file in files_a:
+        value_original = x_file(file)
+        comp_file0(files_b[pos],'Resultados_0/'+file[9:12]+'_A-B.csv',value_original)
+        comp_file0(files_c[pos], 'Resultados_0/'+file[9:12]+'_A-C.csv',value_original)
+        pos =pos+1
+    pos = 0
+    for file in files_b:
+        value_original = x_file(file)
+        comp_file0(files_c[pos], 'Resultados_0/'+file[9:12]+'_B-C.csv',value_original)
+        pos =pos+1
+
+def NormBE(o_filename,all_files_nmat,files_a,files_b,files_c):
+    value_original = o_file(o_filename)
+    for file in all_files_nmat:
+        comp_fileBE(file, 'Resultados_BE/'+file[9:12]+'_o-'+file[13:14]+'.csv',value_original)
+    pos = 0
+    for file in files_a:
+        value_original = o_file(file)
+        comp_fileBE(files_b[pos],'Resultados_BE/'+file[9:12]+'_A-B.csv',value_original)
+        comp_fileBE(files_c[pos], 'Resultados_BE/'+file[9:12]+'_A-C.csv',value_original)
+        pos =pos+1
+    pos = 0
+    for file in files_b:
+        value_original = o_file(file)
+        comp_fileBE(files_c[pos], 'Resultados_BE/'+file[9:12]+'_B-C.csv',value_original)
+        pos =pos+1
+
+def comp_fileBE(comp_filename, w_filename, value_original):
+    with open(comp_filename, 'r') as compare, open(w_filename, 'w', newline='') as result:
+        compare_reader = csv.reader(compare)
+        result_writer = csv.writer(result)
+        result_writer.writerow(['OnSet desviation'])
+        fila = 1
+        min = float(value_original[1])
+        max = float(value_original[-1])
         next(compare_reader)
         for linia in compare_reader:
             if isinstance(float(linia[5]), float):
@@ -27,10 +96,10 @@ def comp_file(comp_filename, w_filename):
         for row in compare_reader:
             fila = fila+1
             a = float(row[5])-float(norm)
+            a_normBE = (a-min)/(max-min)
             b = value_original[fila]
-            resultado = a-float(b)
+            resultado = a_normBE-float(b)
             result_writer.writerow([resultado])
-
 
 
 o_filename = 'CsvFiles/Miniature1.csv'
@@ -38,21 +107,8 @@ all_files_nmat = glob.glob('CsvFiles/*T_nmat.csv')
 files_a = glob.glob('CsvFiles/*AT_nmat.csv')
 files_b = glob.glob('CsvFiles/*BT_nmat.csv')
 files_c = glob.glob('CsvFiles/*CT_nmat.csv')
-value_original = o_file(o_filename)
-for file in all_files_nmat:
-    comp_file(file, 'Resultados/'+file[9:12]+'_o-'+file[13:14])
-pos = 0
-for file in files_a:
-    value_original = o_file(file)
-    comp_file(files_b[pos],'Resultados/'+file[9:12]+'_A-B')
-    comp_file(files_c[pos], 'Resultados/'+file[9:12]+'_A-C')
-    pos =pos+1
-pos = 0
-for file in files_b:
-    value_original = o_file(file)
-    comp_file(files_c[pos], 'Resultados/'+file[9:12]+'_B-C')
-    pos =pos+1
-
+Norm0(o_filename,all_files_nmat,files_a,files_b,files_c)
+#NormBE(o_filename,all_files_nmat,files_a,files_b,files_c)
 
 
 
